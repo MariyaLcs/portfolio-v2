ebfd8714ac6708ef894dbd8579ed3f3c454e2053
Mariya Podosinova <mariya.podosinova@gmail.com>
2024-04-22 10:23:53 +0100

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import styles from "../styles/index.module.css";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to My Portfolio</h1>
+      <p>This is the homepage of my new portfolio website.</p>
+    </div>
+  );
+}
